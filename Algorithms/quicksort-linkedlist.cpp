7b6c02b7d8276ef37f2d5ddd2aa4346f48954564
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2011-02-05 22:22:47 -0500

    previous quicksort was an old version, replaced it with submitted version

--- Algorithms/quicksort-linkedlist.cpp
+++ Algorithms/quicksort-linkedlist.cpp
@@ -1,3 +1,4 @@
+//Carl Chinatomby - CS220 Algorithms - FALL 2009 - Prof. Brass - Section M
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -5,15 +6,13 @@ using namespace std;
 struct listnode { int value; struct listnode *next;};
 
 void list_head_insert(listnode*& head_ptr, int value, int& length){
-	listnode* newnode = new listnode;
-	newnode->value=value;
-	newnode->next = head_ptr;
+	listnode* newnode = new listnode;	newnode->value=value;	newnode->next = head_ptr;
 	head_ptr=newnode;
 	length++;
 }
 
 void list_remove(listnode* previous_ptr, int& length){
-	if (previous_ptr!= NULL){
+	if (length > 0){
 		listnode* remove = previous_ptr->next;
 		previous_ptr -> next = previous_ptr->next->next;
 		delete remove;
@@ -21,11 +20,8 @@ void list_remove(listnode* previous_ptr, int& length){
 	}
 }
 
-
-
-
 void list_head_remove(listnode* &head_ptr, int& length){
-	if (head_ptr != NULL){
+	if (length > 0){
 		listnode* remove = head_ptr;
 		head_ptr = head_ptr->next;
 		delete remove;
@@ -33,33 +29,29 @@ void list_head_remove(listnode* &head_ptr, int& length){
 	}
 }
 
-void print(listnode* head_ptr)
-{
+listnode* findLastNode(listnode* head_ptr){
+	listnode* last(NULL);
 	if (head_ptr!=NULL)
 		for (listnode* cursor=head_ptr; cursor!=NULL; cursor=cursor->next){
-			cout << cursor -> value << " ";
+			if (cursor->next == NULL)
+				last = cursor;
 		}
-	cout << endl;
-	cout << endl;
+	return last;
 }
-
 struct listnode *quicksort(struct listnode *data, int length){
 	listnode *pivot_ptr=NULL, *biggerList=NULL, *smaller_tail_ptr=data;
-	if (length > 1){
+
+	if (length > 1){ 
 		int pivotValue, pivotPosition = rand()%length+1, biggerLength(0);
-
-
 		//first pass locate pivot
 		listnode* cursor = data;
 		for (int i=1; i <= pivotPosition; i++){
-			if (i+1==pivotPosition || pivotPosition==1){
-				pivot_ptr = new listnode;
+			if (i+1==pivotPosition || pivotPosition==1){ //if pivot is next or if pivot is first item
+				pivot_ptr = new listnode; //create list of pivot alone
 				pivot_ptr->value = cursor->next->value;
 				pivot_ptr->next= cursor->next->next; 
-
 				pivotValue = pivot_ptr->value;
-				cout << "Pivot Value is: " << pivotValue << endl;
-				list_remove(cursor, length);
+				list_remove(cursor, length); //remove pivot from list
 			}
 			else
 				cursor = cursor->next;
@@ -68,14 +60,11 @@ struct listnode *quicksort(struct listnode *data, int length){
 		//partition list into two lists, smaller than and bigger than list
 
 		//check nodes 2 to length
-		for (cursor = data; cursor!=NULL && cursor->next != NULL; cursor = cursor->next) {
-
+		for (cursor = data; cursor!=NULL; cursor = cursor->next) {
 			while (cursor->next != NULL && cursor->next->value > pivotValue){
 				list_head_insert(biggerList, cursor->next->value, biggerLength);
 				list_remove(cursor, length);
 			}
-			if (cursor->next== NULL)
-				smaller_tail_ptr = cursor;
 		}
 		//Check Head Node
 		if (data!= NULL && data-> value > pivotValue) {
@@ -83,53 +72,34 @@ struct listnode *quicksort(struct listnode *data, int length){
 			list_head_remove(data, length);
 		}
 
-		cout << "Printing Smaller List" << endl;
-		print(data);
-		cout << "Printer Bigger List" << endl;
-		print(biggerList);
-
-
 		//recursive calls
-
 		data=quicksort(data, length);
-		smaller_tail_ptr->next = pivot_ptr;
-		pivot_ptr->next=quicksort(biggerList, biggerLength);
 
+		if (length > 0){
+			smaller_tail_ptr=findLastNode(data);
+			smaller_tail_ptr->next=pivot_ptr;}
+		else
+			data=pivot_ptr;
 
-/*
-		cout << "Sorting SMALLER LIST!!!" << endl;
-		
-
-
-		cout << "Sorting BIGGER LIST!!!" << endl;
-//		quicksort(pivot_ptr->next, biggerLength);
-		
-		
-		quicksort(biggerList, biggerLength);
-
-		//combine
-		
-		
-		if (biggerList !=NULL)
-			pivot_ptr->next = biggerList;
-		
-*/
-		cout << "PRINTING MERGED LIST" << endl;
-		print(data);
+		if (biggerLength > 0)
+			pivot_ptr->next=quicksort(biggerList, biggerLength);
+		else
+			pivot_ptr->next=NULL;
 	}//if
 	return data;
 }//quicksort
 
+
 int main()
 {
-	const int CAPACITY = 10;
+	const int CAPACITY = 100;
 	listnode* head_ptr = NULL;
 	int length(0);
 
 	srand(time(NULL));
 
-//	quicksort (head_ptr,length); // sort empty list
-//	cout << endl;
+	quicksort (head_ptr,length); // sort empty list
+	cout << endl;
 
 	for (int i=0; i<CAPACITY; i++){
 		list_head_insert(head_ptr, rand()%CAPACITY, length);
@@ -140,7 +110,7 @@ int main()
 	}
 	cout << endl;
 
-	quicksort(head_ptr, length);
+	head_ptr=quicksort(head_ptr, length);
 
 	for (listnode* cursor=head_ptr; cursor!=NULL; cursor=cursor->next){
 		cout << cursor -> value << " ";
