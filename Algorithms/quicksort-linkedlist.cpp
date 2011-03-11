0429b01038addedff53354bd60d407d22dbd5dc0
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2011-03-11 01:23:51 -0500

    added comments to quicksort-linkedlist.cpp

--- Algorithms/quicksort-linkedlist.cpp
+++ Algorithms/quicksort-linkedlist.cpp
@@ -1,16 +1,38 @@
-//Carl Chinatomby - CS220 Algorithms - FALL 2009 - Prof. Brass - Section M
+/*
+ * ***********************
+ * By Carl Chinatomby
+ * ***********************
+ * CS220 Algorithsm - FALL 2009 - Prof. Brass - Section M
+ * 
+ * The task was to implement quicksort using linkedlists 
+ * for the given function prototype and the given listnode 
+ * definition. We were not allowed to copy the data into an 
+ * array and sort that way. Must manipulate linked lists and 
+ * pointers throughout the whole process. The pivot must also
+ * be determined randomly at runtime.
+ * 
+ * Given prototypes:
+ * struct listnode { int value; struct listnode *next;};
+ * struct listnode *quicksort(struct listnode *data, int length);
+ * 
+*/
 #include <iostream>
 #include <ctime>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 
+//This is the listnode definition that was given 
 struct listnode { int value; struct listnode *next;};
 
+//Inserts element to the head of the list
 void list_head_insert(listnode*& head_ptr, int value, int& length){
 	listnode* newnode = new listnode;	newnode->value=value;	newnode->next = head_ptr;
 	head_ptr=newnode;
 	length++;
 }
 
+//Removes an element from the list
 void list_remove(listnode* previous_ptr, int& length){
 	if (length > 0){
 		listnode* remove = previous_ptr->next;
@@ -20,6 +42,7 @@ void list_remove(listnode* previous_ptr, int& length){
 	}
 }
 
+//Removes the head element of the list
 void list_head_remove(listnode* &head_ptr, int& length){
 	if (length > 0){
 		listnode* remove = head_ptr;
@@ -29,6 +52,7 @@ void list_head_remove(listnode* &head_ptr, int& length){
 	}
 }
 
+//Returns a pointer to the tail node of the list
 listnode* findLastNode(listnode* head_ptr){
 	listnode* last(NULL);
 	if (head_ptr!=NULL)
@@ -38,6 +62,9 @@ listnode* findLastNode(listnode* head_ptr){
 		}
 	return last;
 }
+
+
+//returns a pointer to the sorted linked list - function prototype was given
 struct listnode *quicksort(struct listnode *data, int length){
 	listnode *pivot_ptr=NULL, *biggerList=NULL, *smaller_tail_ptr=data;
 
@@ -55,7 +82,7 @@ struct listnode *quicksort(struct listnode *data, int length){
 			}
 			else
 				cursor = cursor->next;
-		}//for
+		}
 
 		//partition list into two lists, smaller than and bigger than list
 
@@ -85,36 +112,38 @@ struct listnode *quicksort(struct listnode *data, int length){
 			pivot_ptr->next=quicksort(biggerList, biggerLength);
 		else
 			pivot_ptr->next=NULL;
-	}//if
+	}
 	return data;
-}//quicksort
-
-
-int main()
-{
-	const int CAPACITY = 100;
-	listnode* head_ptr = NULL;
-	int length(0);
-
-	srand(time(NULL));
-
-	quicksort (head_ptr,length); // sort empty list
-	cout << endl;
-
-	for (int i=0; i<CAPACITY; i++){
-		list_head_insert(head_ptr, rand()%CAPACITY, length);
-	}
-
-	for (listnode* cursor=head_ptr; cursor!=NULL; cursor=cursor->next){
-		cout << cursor -> value << " ";
-	}
-	cout << endl;
-
-	head_ptr=quicksort(head_ptr, length);
-
-	for (listnode* cursor=head_ptr; cursor!=NULL; cursor=cursor->next){
-		cout << cursor -> value << " ";
-	}
-	cout << endl;
-
 }
+
+int main(void)
+{  /*
+   This is the professor's test code
+   Scores were either pass or fail if the test
+   code succeeded and the program did not perform
+   longer than O(nlogn)
+   */
+   int i;
+   struct listnode *node, *tmpnode; node = NULL;
+   for( i=0; i <100000; i++)
+   { 
+       tmpnode = node;
+       node = (struct listnode *) malloc( sizeof(struct listnode));
+       node->next = tmpnode; node->value = (29*i + 53124)%100000; 
+   }
+   printf("\n sorting \n");
+   node = quicksort(node, 100000);
+   tmpnode = node; i=0;
+   while( tmpnode != NULL )
+   { if (tmpnode->value != i )
+     {  printf("sorting failed at i=%d\n", i);
+        exit(0);
+     }
+     tmpnode = tmpnode->next; i = i+1;
+   }
+   printf("Sorting succeeded\n");
+   exit(0);
+}
+
+
+
