c260dc18968cb0ae3b746ff478654fe3eb10635d
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2017-04-04 20:06:13 -0400

    Setup base framework for Problem C.

---
+++ programing_problems/google_code_jam/2016_qualifier/C-dijkstra/test_dijkstra.py
@@ -0,0 +1,52 @@
+import unittest
+
+from dijkstra import (
+    is_string_reducable,
+    YES,
+    NO
+)
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.errors = []
+
+    def tearDown(self):
+        if self.errors:
+            raise AssertionError(self.errors)
+
+    def run_table_test(self, test_cases, test_fct):
+        for test_input, expected_value in test_cases:
+            actual_value = test_fct(test_input)
+            try:
+                self.assertEqual(expected_value, actual_value)
+            except AssertionError:
+                error_msg = "Input: {test_input} Expected: " \
+                    "{expected_value} Actual: {actual_value}".format(
+                        **locals()
+                    )
+                self.errors.append(error_msg)
+
+
+class TestDijkstra(BaseTestCase):
+    def setUp(self):
+        self.errors = []
+
+    def tearDown(self):
+        if self.errors:
+            raise AssertionError(self.errors)
+
+    def test_is_string_reducable(self):
+        test_cases = (
+            # input, output
+            ('ik', NO),
+            ('ijk', YES),
+            ('kji', NO),
+            ('ji'*6, YES),
+            ('i'*10000, NO),
+        )
+        self.run_table_test(test_cases, is_string_reducable)
+
+
+if __name__ == "__main__":
+    unittest.main()
