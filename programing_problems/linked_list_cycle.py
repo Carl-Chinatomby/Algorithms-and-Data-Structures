828e7448cc8ba7685adf64cbb17af0730264f0f4
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2020-05-13 20:05:00 -0700

    Updated comments on linked_list_cycle.

--- programing_problems/linked_list_cycle.py
+++ programing_problems/linked_list_cycle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
 """
-https://leetcode.com/problems/squares-of-a-sorted-array/
-Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
-
-
-
-Example 1:
-
-Input: [-4,-1,0,3,10]
-Output: [0,1,9,16,100]
-Example 2:
-
-Input: [-7,-3,2,3,11]
-Output: [4,9,9,49,121]
-
-
-Note:
-
-1 <= A.length <= 10000
--10000 <= A[i] <= 10000
-A is sorted in non-decreasing order.
+https://leetcode.com/problems/linked-list-cycle/
 """
 from collections import deque
 from typing import List
