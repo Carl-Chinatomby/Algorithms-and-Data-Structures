7cf895461c8c0b8c7d0eaa1d97b77b8f736dce47
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2020-05-19 14:56:15 -0700

    Removed unnecessary comments.

--- programing_problems/binary_tree_height_cycle.py
+++ programing_problems/binary_tree_height_cycle.py
@@ -1,12 +1,3 @@
-# Libraries Included:
-# Numpy, Scipy, Scikit, Pandas
-# 
-# Old Content below(Python 2):
-# 
-# # Libraries Included:
-# # Numpy, Scipy, Scikit, Pandas
-# 
-# print "Hello, world!"
 import uuid
 import unittest
 from copy import deepcopy
@@ -18,13 +9,13 @@ class Node():
             self.children = []
         else:
             self.children = children
-            
+
     def add_child(self, node):
         self.children.append(node)
-        
+
     def add_children(self, nodes):
         self.children.extend(nodes)
-    
+
 def get_children(root):
     remaining_nodes = [root]
     while remaining_nodes:
@@ -32,31 +23,31 @@ def get_children(root):
         yield current_node
         for child in current_node.children:
             remaining_nodes.append(child)
-    
+
 def calculate_height(root):
     current_node = root
     count = -1 # do not count root
-    
+
     if current_node is None:
         return 0
-    
+
     for node in get_children(current_node):
         count += 1
-    
+
     return count
 
 def has_cycle(root):
     if not root or not root.children:
         return False
-    
+
     has_visited = {}
-    
+
     for current_node in get_children(root):
         if has_visited.get(current_node.id):
             return True
 
         has_visited[current_node.id] = True
-    
+
     return False
 
 
@@ -67,12 +58,12 @@ def create_loop_nodes():
     return base_node
 
 def generate_nodes(n):
-    return [Node() for i in range(n)] 
+    return [Node() for i in range(n)]
 
 class TestHasCycle(unittest.TestCase):
     def generate_nodes(self, n):
-        return [Node() for i in range(n)]      
-    
+        return [Node() for i in range(n)]
+
     def setUp(self):
         self.circular = Node(
         children=[
@@ -80,36 +71,36 @@ class TestHasCycle(unittest.TestCase):
         ])
         self.circular.add_children(generate_nodes(8))
         self.circular.children[0].add_children(generate_nodes(3))
-            
+
         self.non_circular = Node()
         self.non_circular.add_children(generate_nodes(8))
         self.non_circular.children[0].add_children(generate_nodes(3))
-    
+
     def test_empty_root_returns_false(self):
         self.assertFalse(has_cycle(None))
-    
+
     def test_single_node_returns_false(self):
         self.assertFalse(has_cycle(Node()))
-    
+
     def test_node_with_multiple_children_for_cycles(self):
         self.assertTrue(has_cycle(self.circular))
         self.assertFalse(has_cycle(self.non_circular))
-    
+
     def test_node_with_multiple_nested_children_for_cycle(self):
         circular_nested = deepcopy(self.circular)
         circular_nested.children[1].add_children(generate_nodes(8))
         self.assertTrue(has_cycle(circular_nested))
-        
+
         non_circular_nested = deepcopy(self.non_circular)
         non_circular_nested.children[1].add_children(generate_nodes(8))
         self.assertFalse(has_cycle(non_circular_nested))
-    
+
 
 def main():
     node1 = Node()
     node2 = Node(children=[node1])
     node1.add_child(node2)
-    
+
     root = Node(
         children=[
             node1,
@@ -117,7 +108,7 @@ def main():
             Node()
         ]
     )
-    
+
     root2 = Node(
         children=[
             Node(),
@@ -125,8 +116,8 @@ def main():
             Node()
         ]
     )
-            
-            
+
+
     print(calculate_height(root))
     print(has_cycle(root2))
 
