02d2fb2f37342958d0c367161b70379ea67dcc49
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2024-09-13 14:40:18 -0400

    Implemented 9_29-regex_matches_str.py

--- programing_problems/ace_the_data_science_interview/9_29-regex_matches_str.py
+++ programing_problems/ace_the_data_science_interview/9_29-regex_matches_str.py
@@ -9,17 +9,55 @@ matches any arbitrary number of characters (empty as well). For example, if the 
 is "abcdba" and the regex is "a*c?*", return true. However, if the regex was instead "b*c?*"
 return false.
 """
-def func(x, y):
-    pass
+# s is string, p is the regex
+def is_match(s: str, p: str) -> bool:
+
+    def dfs(i, j):
+        if i >= len(s) or j >= len(p):
+            return True
+        if j >= len(p):
+            return False
+
+        match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+        if (j+1 < len(p)) and p[j+1] == '*':
+            return (dfs(i, j+2) or  # don't use star
+                (match and dfs(i+1, j)))  # use star and the existing char matches
+
+        if match:
+            return dfs(i+1, j+1)
+
+        return False
+
+    return dfs(0, 0)
 
 
 if __name__ == "__main__":
-    arr1 = []
-    arr2 = []
-    expected_value = 0
+    s = 'aa'
+    p = 'a'
+    expected_value = False
 
-    actual_value = func(arr1, arr2)
+    actual_value = is_match(s, s)
     print("Test 1:", actual_value == expected_value,
+        "actual:", actual_value,
+        "expected:", expected_value
+    )
+
+    s = 'aa'
+    p = 'a*'
+    expected_value = True
+
+    actual_value = is_match(s, s)
+    print("Test 2:", actual_value == expected_value,
+        "actual:", actual_value,
+        "expected:", expected_value
+    )
+
+    s = 'ab'
+    p = '.*'
+    expected_value = True
+
+    actual_value = is_match(s, s)
+    print("Test 3:", actual_value == expected_value,
         "actual:", actual_value,
         "expected:", expected_value
     )
