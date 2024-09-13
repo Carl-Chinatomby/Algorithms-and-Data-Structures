d02865c6a48178827c351b71fd789cdc42411613
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2024-09-13 13:56:19 -0400

    Fixed indexing bug in 9_21-max_common_subarray.py

--- programing_problems/ace_the_data_science_interview/9_21-max_common_subarray.py
+++ programing_problems/ace_the_data_science_interview/9_21-max_common_subarray.py
@@ -11,8 +11,8 @@ def longest_common(a, b):
     dp = [[0 for i in range(len(a)+1)] for j in range(len(b)+ 1)] # initialize all to 0
 
     max_val = 0
-    for i in range(len(a) + 1):
-        for j in range(len(b) + 1):
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
             if a[i-1] == b[j-1]:
                 dp[i][j] = 1 + dp[i-1][j-1] # update dp[i][j]
                 max_val = max(max_val, dp[i][j])
